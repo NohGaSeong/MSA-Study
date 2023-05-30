f59c4d807ac021fe9762b88bf9b087288012deaf
yohan <noyohanx@naver.com>
2023-05-30 19:54:34 +0900

    :sparkles: :: create consumer

---
+++ order/consumer.py
@@ -0,0 +1,21 @@
+import pika 
+
+params = pika.URLParameters('amqps://igqylvwy:TcwMgVG-nqWB4Riz7lSMPp17hEg3qOAC@vulture.rmq.cloudamqp.com/igqylvwy')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='order')
+
+def callback(ch, method, properties, body):
+    print('Received in order')
+    print(body)
+
+channel.basic_consume(queue='order', on_message_callback=callback)
+
+print('Started consuming')
+
+channel.start_consuming()
+
+channel.close()
