5cd831c56b9ce38fbf98ca4b5c4ca8f925400d7d
yohan <noyohanx@naver.com>
2023-05-30 18:17:26 +0900

    :sparkles: :: db 관련 작업 완료

--- boss/main.py
+++ boss/main.py
@@ -1,26 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from dataclasses import dataclass
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 CORS(app)
 
 db = SQLAlchemy(app)
 
+@dataclass
 class Shop():
-    id = db.Column(db.Integer, primary_key = True, autoincrement=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     shop_name = db.Column(db.String(200))
     shop_address = db.Column(db.String(200))
 
+@dataclass
 class Order():
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     shop = db.Column(db.Integer)
     address = db.Column(db.String(200))
 
-@app.route("/")
-def index():
-    return 'hello'
-
-if __name__ == '__main__':
+if __name__ =='__main__':
     app.run(debug=True, host='0.0.0.0')
